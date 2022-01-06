be3a5965cfed02a9fded67d17363f03cf51a3e02
shaastri <shastri.06@gmail.com>
2022-01-06 08:05:41 +0530

    My New Porject

---
+++ genesis.js
@@ -0,0 +1,8 @@
+const genesis_info = {
+    timestamp: new Date(),
+    previousHash: '1747e5e9de9e6935f00a13c56ef1183e0c6129a3fd5b1bc1b25117481fb01453',
+    currentHash: '21ed63d836d1689019032b1dd0cfb859ae865a0cc4785569b972827fdd880adc',
+    data: 'Block Number : 0'
+};
+
+module.exports = { genesis_info };
